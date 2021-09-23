22f091a8720340199e88498aa45e664cd4ed4817
neera-j <86142113+neera-j@users.noreply.github.com>
2021-09-23 18:02:22 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -153,7 +153,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f💠 Page 1/{len_result if len_result < max_pages else max_pages} 💠", callback_data="ignore")
+            InlineKeyboardButton(f"💠 Page 1/{len_result if len_result < max_pages else max_pages} 💠", callback_data="ignore")
         ])
         
         
